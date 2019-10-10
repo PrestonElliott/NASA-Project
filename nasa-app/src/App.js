1539c5b506a77de8ef0268b23869aa9632ff166a
Preston Elliott <preston.elliott24@gmail.com>
2019-10-10 09:10:53 -0400

    working on daily image page design

--- nasa-app/src/App.js
+++ nasa-app/src/App.js
@@ -4,38 +4,45 @@ import DailyPic from './Components/dailyPic';
 
 class App extends React.Component {
 
-  state = {
-    data: {
-        date: "",
-        explanation: "",
-        hdurl: "",
-        media_type: "",
-        service_version: "",
-        title: "",
-        url: ""
+	state = {
+		data: {
+			date: "",
+			explanation: "",
+			hdurl: "",
+			media_type: "",
+			service_version: "",
+			title: "",
+			url: ""
+		},
+		showHdImage: false
+	}
+
+    displayHdImage = (e) => {
+        this.setState({ ...this.state, showHdImage: !showHdImage })
+
     }
-  }
 
-  fetchDailyPic () {
-      const key = "Twc8x8c9cA57VmYOa3boYeqTce88Oig9md5mhjmC"
-      fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
-      .then(res => res.json())
-      .then(data => {console.log(data)
-       this.setState({data: data})
-    })
-  }
+	fetchDailyPic () {
+		const key = "Twc8x8c9cA57VmYOa3boYeqTce88Oig9md5mhjmC"
+		fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+			this.setState({data: data})
+		})
+	}
 
-  componentDidMount() { 
-      this.fetchDailyPic()
-  }
+	componentDidMount() { 
+		this.fetchDailyPic()
+	}
 
-  render() {
-      return (
-          <div>
-              <DailyPic data={this.state.data}/>
-          </div>
-      )
-  }
+	render() {
+		return (
+			<div>
+				<DailyPic data={this.state.data} displayHdImage={this.displayHdImage}/>
+			</div>
+		)
+	}
 }
 
 export default App;
--- nasa-app/src/Components/dailyPic.js
+++ nasa-app/src/Components/dailyPic.js
@@ -1,16 +1,30 @@
 import React, { Fragment } from 'react'
-// import Pic from './pic'
+import { Image, Jumbotron, Button } from 'react-bootstrap'
 
-const DailyPic = (props) => {
-    return (
-        <Fragment>
-            <div>
-                <p>{props.data.explanation}</p>
-                <p>{props.data.title}</p>
-                <img alt="not loaded" src={props.data.hdurl} />
-            </div>
-        </Fragment>
-    )
+// UPDATED TO CLASS COMPONENT
+// const DailyPic = (props) => {
+
+class DailyPic extends React.Component {
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <Jumbotron>
+                        <h2>{this.props.data.title}</h2>
+                        <p>{this.props.data.explanation}</p>
+                        <p>
+                        <Button onClick={(e) => this.displayHdImage(e)} variant="primary">Show HD Image</Button>
+                        </p>
+                    </Jumbotron>
+                </div>
+
+                <div>
+                    <Image src={this.props.data.url} fluid />
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default DailyPic 
