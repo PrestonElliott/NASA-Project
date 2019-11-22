cc419c0c7eddfe84d48f3840ea7cdb76d0d487d2
Preston Elliott <preston.elliott24@gmail.com>
2019-11-22 16:11:03 -0500

    added state to epic nasa component

--- nasa-app/src/App.js
+++ nasa-app/src/App.js
@@ -13,10 +13,7 @@ export default class App extends React.Component {
 
 	state = {
 		dailyPic: {
-
 		},
-		epicPicsData: {},
-		imgUrlArr: [],
 		startDate: new Date(2015, 7, 20)	
 	}
 
@@ -34,39 +31,6 @@ export default class App extends React.Component {
 
 	// CONTINUE TO WORK ON THIS FETCH
 	// Valid dateRange Values: "2015-06-13" through "2019-06-27"
-	fetchEpicDate() {
-		const key = process.env.REACT_APP_NASA_API_KEY
-		
-		let d = this.state.startDate
-		let dateFormat = 
-			d.getFullYear() 
-			+ "-" + ("0" + (d.getMonth() + 1)).slice(-2) 
-			+ "-" + ("0" + d.getDate()).slice(-2)
-		console.log(dateFormat)
-
-		fetch(`https://api.nasa.gov/EPIC/api/natural/date/${dateFormat}?api_key=${key}`)
-		.then(res => res.json())
-		.then(data => {
-			if(this.state.data !== data.data) {
-				this.setState({ ...this.state, epicPicsData: data }) 
-				this.constructEpicImgUrl(data)
-			}
-		})
-	}
-
-	constructEpicImgUrl = (epicPicsData) => {
-		epicPicsData.forEach(epic => {
-			
-			let dateFormat = ""
-			let img = ""
-	
-			dateFormat = epic.date.split(" ")[0].replace("-", "/").replace("-", "/")
-			img = epic.image
-
-			let newImgUrl = (`https://epic.gsfc.nasa.gov/archive/natural/${dateFormat}/png/${img}.png`) 
-			this.setState({ ...this.state, imgUrlArr: [...this.state.imgUrlArr, newImgUrl ] })
-		})
-	}
 	
 	handleChange = (date) => { this.setState({ ...this.state, startDate: date }) }
 
@@ -74,9 +38,9 @@ export default class App extends React.Component {
 		console.log(this.state)
 		return (
 			<div>
-			<DailyPic />
-			{/* <DateSelect selected={this.state.startDate} onChange={this.handleChange}/>
-			<EpicNasa imgUrlArr={this.state.imgUrlArr}  /> */}
+				<DailyPic />
+				{/* <DateSelect selected={this.state.startDate} onChange={this.handleChange}/> */}
+				<EpicNasa />
 			</div>
 		)
 	}
--- nasa-app/src/Components/epicNasa.jsx
+++ nasa-app/src/Components/epicNasa.jsx
@@ -3,13 +3,51 @@ import React, { Component } from 'react'
 import EpicNasaCard from './epicNasaCard'
 
 export default class EpicNasa extends Component {
+
+    state = {
+        epicPicsData: {},
+		imgUrlArr: []
+    }
+
+    fetchEpicDate() {
+		const key = process.env.REACT_APP_NASA_API_KEY
+		
+		let d = this.state.startDate
+		let dateFormat = 
+			d.getFullYear() 
+			+ "-" + ("0" + (d.getMonth() + 1)).slice(-2) 
+			+ "-" + ("0" + d.getDate()).slice(-2)
+		console.log(dateFormat)
+
+		fetch(`https://api.nasa.gov/EPIC/api/natural/date/${dateFormat}?api_key=${key}`)
+		.then(res => res.json())
+		.then(data => {
+			if(this.state.data !== data.data) {
+				this.setState({ ...this.state, epicPicsData: data }) 
+				this.constructEpicImgUrl(data)
+			}
+		})
+	}
+
+	constructEpicImgUrl = (epicPicsData) => {
+		epicPicsData.forEach(epic => {
+			
+			let dateFormat = ""
+			let img = ""
+	
+			dateFormat = epic.date.split(" ")[0].replace("-", "/").replace("-", "/")
+			img = epic.image
+
+			let newImgUrl = (`https://epic.gsfc.nasa.gov/archive/natural/${dateFormat}/png/${img}.png`) 
+			this.setState({ ...this.state, imgUrlArr: [...this.state.imgUrlArr, newImgUrl ] })
+		})
+	}
     
     render() {
-
         return (
             <div>
-                {this.props.imgUrlArr.length &&
-                this.props.imgUrlArr.map(imgUrl => 
+                {this.state.imgUrlArr.length &&
+                this.state.imgUrlArr.map(imgUrl => 
                     imgUrl === null ? 
                     "No photos were taken on this date." :  <EpicNasaCard key={imgUrl.id} imgUrl={imgUrl} /> 
                     )
